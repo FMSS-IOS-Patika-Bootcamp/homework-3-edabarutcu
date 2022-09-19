2e1c4f9edf66ed44e01f1bc83968fb24d2b8f3ec
edabarutcu <edabarutcu@protonmail.com>
2022-09-19 11:44:53 +0300

    -post model created

---
+++ Homework3/Models/PostModel.swift
@@ -0,0 +1,22 @@
+//
+//  PostModel.swift
+//  Homework3
+//
+//  Created by detaysoft on 19.09.2022.
+//
+
+import Foundation
+
+
+struct Post: Decodable {
+    var userID: Int
+    var id: Int
+    var title: String
+    var body: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case id, title, body
+    }
+    
+}
---
+++ Homework3/Views/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Homework3
+//
+//  Created by detaysoft on 19.09.2022.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
