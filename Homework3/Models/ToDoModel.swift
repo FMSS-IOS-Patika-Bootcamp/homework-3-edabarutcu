d31ad7781ea3aaddd98e662e68f373ad83f4fd5a
edabarutcu <edabarutcu@protonmail.com>
2022-09-20 16:23:46 +0300

    -added core data model and constraint fixed

---
+++ Homework3/Models/ToDoModel.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoModel.swift
+//  Homework3
+//
+//  Created by detaysoft on 20.09.2022.
+//  Copyright © 2022 Eda Barutçu. All rights reserved.
+//
+
+import Foundation
+
+class ToDoModel {
+    var name: String = ""
+    var isImportant: Bool = false
+}
